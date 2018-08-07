551b87aca24808c41dea58b4a12106311d89edba
Marcel Cruz <marcelcruz@live.com>
2018-08-07 16:21:32 +0100

    lodash added

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,6 @@
 console.log('Starting app.js')
 
 const fs = require('fs')
-const os = require('os')
+const _ = require('lodash')
+
 const notes = require('./notes')
-
-const res = notes.addNote()
-console.log(res)
-
-console.log(notes.add(1, 2))
-
-// const user = os.userInfo()
-
-// fs.appendFileSync(
-//   'greetings.txt',
-//   `Hello ${user.username}! You are ${notes.age}.`,
-// )
