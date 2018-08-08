8d1e49cd6b36d657a396a8ae84c7590e4f4eca13
Marcel Cruz <marcelcruz@live.com>
2018-08-08 17:10:10 +0100

    google geocoding added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,17 @@
 const request = require('request')
 
 const address = '14%20old%20county%20glen%20dublin'
-const key = 'AIzaSyCfOifHw7VLrDU5L-wjOqxOwUgvjAHoHL8'
+const key = 'AIzaSyB-tt9YkG6ydI-zlR5pGLMLvTkbhzaYldE'
 
 request(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}}`,
+    // url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`,
     json: true,
   },
-  (error, request, body) => {
-    // if (error) {
-    //   console.log(error)
-    // }
-
-    console.log(body)
+  (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
   },
 )
