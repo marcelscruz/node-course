ae67511b8dd512f4c61c83a3e4464bbf0e7c9599
Marcel Cruz <marcelcruz@live.com>
2018-08-09 22:46:09 +0100

    example tests done

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,75 @@
+const expect = require('expect')
+
 const utils = require('./utils')
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11)
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res)
+    .toBe(44)
+    .toBeA('number')
+})
+
+// done as argument tells mocha the test is async, so should wait to check
+it('should async add two numbers', done => {
+  utils.asyncAdd(4, 3, sum => {
+    expect(sum)
+      .toBe(7)
+      .toBeA('number')
+    done() // mocha processes after this call
+  })
 })
 
 it('should square a number', () => {
   const res = utils.square(3)
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`)
+  expect(res)
+    .toBe(9)
+    .toBeA('number')
+})
+
+it('should async square a number', done => {
+  utils.asyncSquare(3, res => {
+    expect(res)
+      .toBe(9)
+      .toBeA('number')
+    done()
+  })
+})
+
+// some examples
+// it('should expect some values', () => {
+// expect(12).toNotBe(2)
+// expect({ name: 'Marcel' }).toBe({ name: 'Marcel' }) // won't work
+// expect({ name: 'Marcel' }).toEqual({ name: 'Marcel' }) // will work
+// expect([2, 3, 4]).toInclude(3)
+// expect([2, 3, 4]).toExclude(5)
+// expect({
+//   name: 'Marcel',
+//   age: 27,
+// }).toInclude({
+//   age: 27,
+// })
+// expect({
+//   name: 'Marcel',
+//   age: 27,
+// }).toExclude({
+//   age: 25,
+// })
+// })
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+
+it('should set firstName and lastName', () => {
+  const user = {
+    location: 'Dublin',
+    age: 27,
   }
+  const res = utils.setName(user, 'Marcel Cruz')
+
+  expect(res).toInclude({
+    firstName: 'Marcel',
+    lastName: 'Cruz',
+  })
 })
